2cf4012c2509f26dfc2663f7b4b9d5a5150c50df
Amaan Ilahi <115573280+ilahiamaan606@users.noreply.github.com>
2023-05-27 14:31:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,19 @@ const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
 
-server.use(cors(*))
-server.use(jsonServer.bodyParser)
+const corsOptions = {
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+};
+
+server.use(cors(corsOptions));
+
+server.use(jsonServer.bodyParser())
 server.use(middlewares)
+
+server.options('*', cors(corsOptions));
+
+server.use(router);
+
 server.use(router)
 
 const PORT = 8000
