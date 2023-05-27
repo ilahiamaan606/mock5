6ac7edf8cf79d6cb4e520a1ec3dc8d4899ef469f
zeeshani26 <ilahiamaan606@gmail.com>
2023-05-27 15:47:18 +0530

    Done

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const JsonServer = require('json-server');
 const cors = require('cors');
 require("dotenv").config();
+
 const server = JsonServer.create();
 const router = JsonServer.router('db.json');
-const middleware = JsonServer.defaults()
+const middleware = JsonServer.defaults();
 
+server.use(cors());
 
 server.use(middleware);
 server.use(router);
 server.use(JsonServer.bodyParser);
 
-server.listen(process.env.port,()=>{
-    console.log('running on port '+process.env.port);
-})
+server.listen(process.env.port, () => {
+  console.log('Running on port ' + process.env.port);
+});
